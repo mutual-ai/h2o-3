39306ff58d293767b40267e7c6d026ecb8dc368a
mdymczyk <dymczyk@gmail.com>
2016-06-20 23:20:23 +0900

    [WIP] HEXDEV-596: finished SSLSocketChannel implementation

--- h2o-core/src/main/java/water/network/SSLSocketChannel.java
+++ h2o-core/src/main/java/water/network/SSLSocketChannel.java
@@ -19,12 +19,15 @@ import java.nio.channels.SocketChannel;
  */
 public class SSLSocketChannel implements ByteChannel {
 
+    // Empty buffer used during SSL/TLS handshake
+    private static ByteBuffer emptyBuffer = ByteBuffer.allocate(0);
+
     // Buffers holding encrypted data
     private ByteBuffer netInBuffer;
     private ByteBuffer netOutBuffer;
 
     // Buffers holding decrypted data
-    // TODO probably can use only 1 buffer for both with some flag
+    // Probably can use only 1 buffer for both with some flag
     private ByteBuffer myAppData;
     private ByteBuffer peerAppData;
 
@@ -46,6 +49,86 @@ public class SSLSocketChannel implements ByteChannel {
     }
 
     @Override
+    public boolean isOpen() {
+        return sc.isOpen();
+    }
+
+    @Override
+    public void close() throws IOException {
+        closing = true;
+        sslEngine.closeOutbound();
+        sslEngine.getSession().invalidate();
+        netOutBuffer.clear();
+        myAppData.clear();
+
+        while (!sslEngine.isOutboundDone()) {
+            sslEngine.wrap(myAppData, netOutBuffer);
+
+            while(netOutBuffer.hasRemaining()) {
+                sc.write(netOutBuffer);
+                netOutBuffer.compact();
+            }
+        }
+
+        sc.close();
+        closed = true;
+    }
+
+    private void prepareBuffers(SSLSession session) {
+        int appBufferSize = session.getApplicationBufferSize();
+        // Less is not more. More is more. Bigger than the app buffer size so successful unwraps() don't cause BUFFER_OVERFLOW
+        myAppData = ByteBuffer.allocate(appBufferSize + 64);
+        peerAppData = ByteBuffer.allocate(appBufferSize + 64);
+
+        int netBufferSize = session.getPacketBufferSize();
+
+        netInBuffer = ByteBuffer.allocate(netBufferSize);
+        netOutBuffer = ByteBuffer.allocate(netBufferSize);
+    }
+
+    // -----------------------------------------------------------
+    // HANDSHAKE
+    // -----------------------------------------------------------
+
+    private void handshake() throws IOException {
+        sslEngine.beginHandshake();
+        SSLEngineResult.HandshakeStatus hs = sslEngine.getHandshakeStatus();
+
+        while(hs != SSLEngineResult.HandshakeStatus.FINISHED &&
+                hs != SSLEngineResult.HandshakeStatus.NOT_HANDSHAKING) {
+            switch(hs){
+                case NEED_WRAP : {
+                    myAppData.clear();
+                    sc.write(wrap(emptyBuffer));
+                }
+                case NEED_UNWRAP:
+                    netInBuffer.clear();
+                    if(sc.read(netInBuffer) > 1) {
+                        unwrap(peerAppData);
+                    }
+                    break;
+                // SSL needs to perform some delegating tasks before it can continue.
+                // Those tasks will be run in the same thread and can be blocking.
+                case NEED_TASK :
+                    hs = tasks();
+                    break;
+            }
+        }
+        resetBuffers();
+    }
+
+    private void resetBuffers() {
+        myAppData.clear();
+        peerAppData.clear();
+        netInBuffer.clear();
+        netOutBuffer.clear();
+    }
+
+    // -----------------------------------------------------------
+    // READ AND WRITE
+    // -----------------------------------------------------------
+
+    @Override
     public int read(ByteBuffer dst) throws IOException {
         if (closing || closed) return -1;
 
@@ -60,36 +143,58 @@ public class SSLSocketChannel implements ByteChannel {
 
     private synchronized int unwrap(ByteBuffer dst) throws IOException {
         int read = 0;
-        SSLEngineResult unwrap;
+        SSLEngineResult unwrapResult;
+        peerAppData.clear();
 
-        do {
+        while(netInBuffer.hasRemaining()) {
             netInBuffer.flip();
 
-            unwrap = sslEngine.unwrap(netInBuffer, dst);
+            unwrapResult = sslEngine.unwrap(netInBuffer, dst);
             netInBuffer.compact();
 
-            if (unwrap.getStatus() == SSLEngineResult.Status.OK ||
-                    unwrap.getStatus() == SSLEngineResult.Status.BUFFER_UNDERFLOW) {
-                read += unwrap.bytesProduced();
+            switch (unwrapResult.getStatus()) {
+                case OK: {
+                    read += unwrapResult.bytesProduced();
 
-                if (unwrap.getHandshakeStatus() == SSLEngineResult.HandshakeStatus.NEED_TASK) {
-                    tasks();
-                }
-
-                if (unwrap.getStatus() == SSLEngineResult.Status.BUFFER_UNDERFLOW) {
+                    if (unwrapResult.getHandshakeStatus() == SSLEngineResult.HandshakeStatus.NEED_TASK) {
+                        tasks();
+                    }
                     break;
                 }
-            } else if (unwrap.getStatus() == SSLEngineResult.Status.BUFFER_OVERFLOW) {
-                if (read > 0) {
+                case BUFFER_OVERFLOW: {
+                    if (sslEngine.getSession().getApplicationBufferSize() > peerAppData.capacity()) {
+                        int appSize = sslEngine.getSession().getApplicationBufferSize();
+                        ByteBuffer b = ByteBuffer.allocate(appSize + peerAppData.position());
+                        peerAppData.flip();
+                        b.put(peerAppData);
+                        peerAppData = b;
+                    } else if(peerAppData.hasRemaining()) {
+                        peerAppData.compact();
+                    } else {
+                        peerAppData.clear();
+                    }
                     break;
-                } else {
-                    // TODO implement
                 }
-            } else {
-                // Something else went wrong
-                throw new IOException("Failed to SSL unwrap with status " + unwrap.getStatus());
+                case BUFFER_UNDERFLOW: {
+                    if (sslEngine.getSession().getPacketBufferSize() > netInBuffer.capacity()) {
+                        int netSize = sslEngine.getSession().getPacketBufferSize();
+                        if (netSize > netInBuffer.capacity()) {
+                            ByteBuffer b = ByteBuffer.allocate(netSize);
+                            netInBuffer.flip();
+                            b.put(netInBuffer);
+                            netInBuffer = b;
+                        }
+                    } else if(netInBuffer.hasRemaining()) {
+                        netInBuffer.compact();
+                    } else {
+                        netInBuffer.clear();
+                    }
+                    break;
+                }
+                default:
+                    throw new IOException("Failed to SSL unwrap with status " + unwrapResult.getStatus());
             }
-        } while(netInBuffer.position() != 0);
+        }
 
         return read;
     }
@@ -107,16 +212,9 @@ public class SSLSocketChannel implements ByteChannel {
         return sc.write(wrap(src));
     }
 
-    private synchronized ByteBuffer wrap(ByteBuffer b) throws IOException {
+    private synchronized ByteBuffer wrap(ByteBuffer src) throws IOException {
         netOutBuffer.clear();
-        SSLEngineResult res = sslEngine.wrap(b, netOutBuffer);
-
-        if (res.getStatus() == SSLEngineResult.Status.OK) {
-            if (res.getHandshakeStatus() == SSLEngineResult.HandshakeStatus.NEED_TASK) tasks();
-        } else {
-            throw new IOException("Failed to SSL wrap with status " + res.getStatus());
-        }
-
+        sslEngine.wrap(src, netOutBuffer);
         netOutBuffer.flip();
         return netOutBuffer;
     }
@@ -129,119 +227,6 @@ public class SSLSocketChannel implements ByteChannel {
         }else {
             return true;
         }
-    }
-
-    @Override
-    public boolean isOpen() {
-        return sc.isOpen();
-    }
-
-    @Override
-    public void close() throws IOException {
-        sslEngine.closeOutbound();
-        sslEngine.getSession().invalidate();
-        netOutBuffer.clear();
-        sc.write(wrap(netOutBuffer));
-        sc.close();
-    }
-
-    private void prepareBuffers(SSLSession session) {
-        int appBufferSize = session.getApplicationBufferSize();
-        myAppData = ByteBuffer.allocate(appBufferSize);
-        peerAppData = ByteBuffer.allocate(appBufferSize);
-
-        int netBufferSize = session.getPacketBufferSize();
-
-        netInBuffer = ByteBuffer.allocate(netBufferSize);
-        netOutBuffer = ByteBuffer.allocate(netBufferSize);
-    }
-
-    // -----------------------------------------------------------
-    // HANDSHAKE
-    // -----------------------------------------------------------
-
-    private SSLEngineResult.HandshakeStatus hs;
-
-    private void handshake() throws IOException {
-        sslEngine.beginHandshake();
-        hs = sslEngine.getHandshakeStatus();
-
-        SSLEngineResult handshakeResult;
-
-        while(hs != SSLEngineResult.HandshakeStatus.FINISHED &&
-                hs != SSLEngineResult.HandshakeStatus.NOT_HANDSHAKING) {
-            switch(hs){
-                case NEED_WRAP : {
-                    try {
-                        handshakeResult = handshakeWrap();
-                    } catch (SSLException e) {
-                        // TODO log
-                        handshakeResult = handshakeWrap();
-                    }
-                    if (handshakeResult.getStatus() == SSLEngineResult.Status.OK) {
-                        if (hs == SSLEngineResult.HandshakeStatus.NEED_TASK)
-                            hs = tasks();
-                    } else if (handshakeResult.getStatus() == SSLEngineResult.Status.CLOSED) {
-                        flush(netOutBuffer);
-                    } else {
-                        throw new IOException("Unexpected status during wrap " + handshakeResult.getStatus());
-                    }
-                    if ( hs != SSLEngineResult.HandshakeStatus.NEED_UNWRAP || (!flush(netOutBuffer)) ) {
-                        // TODO implement
-                        return;
-                    }
-                }
-                case NEED_UNWRAP:
-                    handshakeResult = handshakeUnwrap();
-                    if ( handshakeResult.getStatus() == SSLEngineResult.Status.OK ) {
-                        if (hs == SSLEngineResult.HandshakeStatus.NEED_TASK) {
-                            hs = tasks();
-                        }
-                    } else if ( handshakeResult.getStatus() == SSLEngineResult.Status.BUFFER_UNDERFLOW ){
-                        // TODO implement
-                        return;
-                    } else if (handshakeResult.getStatus() == SSLEngineResult.Status.BUFFER_OVERFLOW) {
-                        // TODO implement
-                    } else {
-                        throw new IOException("Unexpected status during unwrap  " + handshakeResult.getStatus());
-                    }
-                    break;
-                case NEED_TASK :
-                    hs = tasks();
-                    break;
-            }
-        }
-    }
-
-    private SSLEngineResult handshakeWrap() throws SSLException {
-        netOutBuffer.clear();
-        SSLEngineResult result = sslEngine.wrap(myAppData, netOutBuffer);
-        netOutBuffer.flip();
-        hs = result.getHandshakeStatus();
-        return result;
-    }
-
-    private SSLEngineResult handshakeUnwrap() throws IOException {
-        if (netInBuffer.position() == netInBuffer.limit()) {
-            netInBuffer.clear();
-        }
-
-        SSLEngineResult result;
-        boolean cont;
-        do {
-            netInBuffer.flip();
-            result = sslEngine.unwrap(netInBuffer, peerAppData);
-            netInBuffer.compact();
-            hs = result.getHandshakeStatus();
-            if ( result.getStatus() == SSLEngineResult.Status.OK &&
-                    result.getHandshakeStatus() == SSLEngineResult.HandshakeStatus.NEED_TASK ) {
-                hs = tasks();
-            }
-
-            cont = result.getStatus() == SSLEngineResult.Status.OK &&
-                    hs == SSLEngineResult.HandshakeStatus.NEED_UNWRAP;
-        } while ( cont );
-        return result;
     }
 
     // -----------------------------------------------------------
